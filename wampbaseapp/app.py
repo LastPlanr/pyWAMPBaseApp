e5939c06e782aaed7844c4661649bfd4f248748c
Cléber Zavadniak <cleber@dronemapp.com>
2019-07-01 20:13:17 -0300

    fall to printing progress if socket closes

--- wampbaseapp/app.py
+++ wampbaseapp/app.py
@@ -11,7 +11,25 @@ class App:
     def init(self, *args, **kwargs):
         pass
 
-    def advance_progress(self, step_name=None):
-        if self.details and self.details.progress:
+    def advance_progress_print(self, step_name=None):
+        print(self.current_step, self.total_steps_count, step_name or '')
+        self.current_step += 1
+
+    def advance_progress_send(self, step_name=None):
+        try:
             self.details.progress((self.current_step, self.total_steps_count, step_name))
-            self.current_step += 1
+        except Exception as ex:
+            print(ex)
+            self.advance_progress = self.advance_progress_print
+            return self.advance_progress(step_name)
+
+        self.current_step += 1
+
+    def advance_progress(self, step_name=None):
+        self.original_advance_progress = self.advance_progress
+        if self.details and self.details.progress:
+            self.advance_progress = self.advance_progress_send
+        else:
+            self.advance_progress = self.advance_progress_print
+
+        return self.advance_progress(step_name)
